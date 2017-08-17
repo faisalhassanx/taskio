88203d59755905ce21b86437d36995bbc258f008
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-17 23:08:54 +0000

    Add devise and milia gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
 
   get '/contact' => 'pages#contact'
-  
-  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
